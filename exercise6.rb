97a0c6a2bc40bfe5b7b4abc5bec2c872a5477fa6
Leigha <leigham314@gmail.com>
2016-06-01 15:08:30 -0400

    Added the method

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,20 @@
 grocery_list = ["cookies", "cereal", "watermelon", "pop-tarts"]
 
-  grocery_list.each do |food|
-  puts "* #{food}"
+grocery_list << "rice"
+
+def list(grocery_list)
+    grocery_list.map do |food|
+      puts "* #{food}"
+  end
 end
+
+puts grocery_list.count
+
+puts list(grocery_list)
+
+
+#2 puts grocery_list.count
+
+#4 puts grocery_list = [1]
+
+#5 grocery_list.sort
